b8a5bca91f11e8251c4f403cf5ea48593b0d7797
Shashi Shekhar <shshekhar93@gmail.com>
2025-07-29 01:49:17 +0530

    Preparing to publish

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
+  globalIgnores(['dist/']),
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
