802bfcb8f21619a5a0594551ac54c9252eb76277
Shashi Shekhar <shshekhar93@gmail.com>
2025-07-29 07:17:48 +0530

    input border width quirkiness

--- src/components/common/form/select.tsx
+++ src/components/common/form/select.tsx
@@ -25,18 +25,28 @@ const Select: React.FC<SelectProps> = ({ options, value, onChange, label, labelA
   }, [id]);
 
   return (
-    <div className={css({ position: 'relative' })}>
+    <div className={css({
+      position: 'relative',
+      border: `${hasFocus ? 2 : 1}px solid ${hasFocus ? 'var(--primary-color)' : 'var(--border-color)'}`,
+      borderRadius: '0.25rem',
+      padding: `${hasFocus ? 0 : 1}px`,
+    })}
+    >
       {label
         && (
           <label
             htmlFor={id}
             className={css({
               position: 'absolute',
-              top: '2px',
-              left: '2px',
-              padding: '0 0.5rem',
+              top: showLargeLabel ? '0.75rem' : '1px',
+              left: hasFocus ? '1px' : '2px',
+              right: hasFocus ? '1px' : '2px',
+              background: 'white',
+              padding: `0 0.5rem`,
               fontSize: showLargeLabel ? '1rem' : '0.8rem',
               fontWeight: 600,
+              transition: 'font-size 0.1s linear, top 0.1s linear',
+              transform: hasFocus ? 'translateY(-1px)' : undefined,
             })}
           >
             {label}
@@ -53,7 +63,7 @@ const Select: React.FC<SelectProps> = ({ options, value, onChange, label, labelA
           width: '100%',
           minWidth: `calc(${minWidth}px + 1rem)`,
           height: '3rem',
-          border: `${hasFocus ? 2 : 1}px solid ${hasFocus ? 'var(--primary-color)' : 'var(--border-color)'}`,
+          borderWidth: 0,
           outline: 'none',
         })}
         onFocus={(e) => {
